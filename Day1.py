7a74e03b2a596aa9b5d2b012e2f41852d657d06d
Desire17 <101705065+Desire17@users.noreply.github.com>
2022-03-16 10:40:18 +0100

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,268 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+Traceback (most recent call last):
+  File "C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py", line 1, in <module>
+    print(osiogbodesire)
+NameError: name 'osiogbodesire' is not defined
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+17
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+99.9
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+99.9
+propser
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+99.9
+propser
+Traceback (most recent call last):
+  File "C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py", line 9, in <module>
+    print(X)
+NameError: name 'X' is not defined
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+99.9
+propser
+True
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+99.9
+propser
+True
+we're brothers from the other side of the town
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+99.9
+propser
+True
+we're brothers from the other side of the town
+we're brothers from the other side of the town
+# i am learning python nd it is fun\were are looking at statement
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+99.9
+propser
+True
+we're brothers from the other side of the town
+we're brothers from the other side of the town
+ i am learning python 
+and it is fun
+were are looking at statement
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+99.9
+propser
+True
+we're brothers from the other side of the town
+we're brothers from the other side of the town
+ i am learning python 
+and it is fun
+were are looking at statement
+
+
+Traceback (most recent call last):
+  File "C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py", line 27, in <module>
+    print(statemnt2)
+NameError: name 'statemnt2' is not defined
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+99.9
+propser
+True
+we're brothers from the other side of the town
+we're brothers from the other side of the town
+Traceback (most recent call last):
+  File "C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py", line 21, in <module>
+    print(statement2)
+NameError: name 'statement2' is not defined
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+99.9
+propser
+True
+we're brothers from the other side of the town
+we're brothers from the other side of the town
+ i am learning python 
+and it is fun
+were are looking at statement
+
+
+Traceback (most recent call last):
+  File "C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py", line 27, in <module>
+    print(statemnt)
+NameError: name 'statemnt' is not defined
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+99.9
+propser
+True
+we're brothers from the other side of the town
+we're brothers from the other side of the town
+ i am learning python 
+and it is fun
+were are looking at statement
+
+
+ i am learning python
+and it is fun
+were are looking at statement
+
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+99.9
+propser
+True
+we're brothers from the other side of the town
+we're brothers from the other side of the town
+ i am learning python 
+and it is fun
+were are looking at statement
+
+
+ i am learning python
+and it is fun
+were are looking at statement
+
+osiogbodesire
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+99.9
+propser
+True
+we're brothers from the other side of the town
+we're brothers from the other side of the town
+ i am learning python 
+and it is fun
+were are looking at statement
+
+
+ i am learning python
+and it is fun
+were are looking at statement
+
+osiogbo+desire
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+99.9
+propser
+True
+we're brothers from the other side of the town
+we're brothers from the other side of the town
+ i am learning python 
+and it is fun
+were are looking at statement
+
+
+ i am learning python
+and it is fun
+were are looking at statement
+
+osiogbo desire
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+99.9
+propser
+True
+we're brothers from the other side of the town
+we're brothers from the other side of the town
+ i am learning python 
+and it is fun
+were are looking at statement
+
+
+ i am learning python
+and it is fun
+were are looking at statement
+
+osiogbo desire
+my name is propser
+>>> 
+== RESTART: C:/Users/GEMINI/Documents/Osiogbo DESIRE PYTTHON BOOT CAMP/Day1.py =
+osiogbodesire
+17
+99.9
+propser
+True
+we're brothers from the other side of the town
+we're brothers from the other side of the town
+ i am learning python 
+and it is fun
+were are looking at statement
+
+
+ i am learning python
+and it is fun
+were are looking at statement
+
+osiogbo desire
+my name is propser
+propser paul
+i sold a shirt for 5000,two pairs of shoe for 18000 and three suit for 15000
+>>> 
+== RESTART: C:\Users\GEMINI\Documents\Osiogbo DESIRE PYTTHON BOOT CAMP\Day1.py =
+osiogbodesire
+17
+99.9
+propser
+True
+we're brothers from the other side of the town
+we're brothers from the other side of the town
+ i am learning python 
+and it is fun
+were are looking at statement
+
+
+ i am learning python
+and it is fun
+were are looking at statement
+
+osiogbo desire
+my name is propser
+propser paul
+i sold a shirt for 5000,two pairs of shoe for 18000 and three suit for 15000
+i sold a shirt for 5000,two pairs of shoe for 18000 and three suit for 15000
+>>> print('my name is james')
